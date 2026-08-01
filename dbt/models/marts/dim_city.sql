d44bc0489c1d219165df507f20cced27b7225194
kromylodd <danilk.demdov@gmail.com>
2026-08-01 08:53:23 +0200

    40 cities prep

--- dbt/models/marts/dim_city.sql
+++ dbt/models/marts/dim_city.sql
@@ -3,9 +3,20 @@ with distinct_cities as (
     select distinct city
     from {{ ref('stg_listings') }}
 
+),
+
+city_lookup as (
+
+    select * from {{ ref('city_lookup') }}
+
 )
 
 select
-    {{ dbt_utils.generate_surrogate_key(['city']) }} as city_key,
-    city
+    {{ dbt_utils.generate_surrogate_key(['distinct_cities.city']) }} as city_key,
+    distinct_cities.city,
+    city_lookup.voivodeship,
+    coalesce(city_lookup.is_mvp, false) as is_mvp
+
 from distinct_cities
+left join city_lookup
+    on distinct_cities.city = city_lookup.city_name
